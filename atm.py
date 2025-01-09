316b9f44416cb582ac8eb7487b1754a5d736ac4c
socical <socical.dev@gmail.com>
2025-01-09 17:10:28 +0900

    🐛 입력 검증 및 에러 처리 추가
    입금 금액에 대한 에러 처리 완료

--- atm.py
+++ atm.py
@@ -1,9 +1,7 @@
-# 출금 요청한 금액을 받는 변수 : withdraw_amount
-# 출금을 요청한 금액을 balance 변수에서 뺀 결과가 들어가도록 코드를 작성해주세요.
-# 영수증에 다음 순으로 값이 들어가도록 코드를 만들어주세요 -> ("출금", withdraw_amount, balance) 순으로 데이터 넣어주세요.
-# 가지고 있는 금액보다 출금을 원하는 금액이 클 때 가지고 있는 금액만 출금되도록 코드를 작성해주세요.
-# 단, 영수증에 내역은 변경되지 않아야 하며 입금 또는 출금이 진행될때마다 이력이 기록됩니다.
-# 영수증 변수는 : receipts
+# 입력 검증 및 에러 처리 추가
+# 잘못된 입력 값(숫자가 아닌 값, 음수 값 등..)을 처리하도록 기능을 추가해주세요.
+# 유효하지 않은 메뉴 선택 시 경고 메시지 또는 사용방법 재안내를 해주세요.
+# gitmoji는 bug 로 선택!!
 
 receipts = []
 balance = 3000
@@ -14,18 +12,21 @@ while True:
     if select_num == "4":
         break
     elif select_num == "1":
-        deposit_amount = int(input("입금하실 금액을 입력해주세요 : "))
-        balance += deposit_amount
-        receipts.append(("입금", deposit_amount, balance))
-        print(f'현재 입금액은 {deposit_amount}원\n현재 잔액은 {balance}원 입니다.')
+        deposit_amount = input("입금하실 금액을 입력해주세요 : ")
+        if deposit_amount.isdigit() and int(deposit_amount) > 0:
+            balance += int(deposit_amount)
+            receipts.append(("입금", deposit_amount, balance))
+            print(f'현재 입금액은 {deposit_amount}원\n현재 잔액은 {balance}원 입니다.')
+        else:
+            print(f'0 이상의 숫자만 입력해주시기 바랍니다.')
     elif select_num == "2":
         withdraw_amount = int(input("출금하실 금액을 입력해주세요 : "))
         # if balance > withdraw_amount:
         #     balance -= balance - withdraw_amount
         # else:
         #     balance = 0
-        # 또는
-        withdraw_amount = min (balance, withdraw_amount)
+        # 또는 min() 함수를 통해 최솟값을 구할 수 있다.
+        withdraw_amount = min(balance, withdraw_amount)
         balance -= withdraw_amount
         receipts.append(("출금", withdraw_amount, balance))
         print(f'현재 출금액은 {withdraw_amount}원\n현재 잔액은 {balance}원 입니다.')
