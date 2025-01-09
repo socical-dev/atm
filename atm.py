07d136880d6ad86e6c6e40c0202c4a7f4b13fc76
socical <socical.dev@gmail.com>
2025-01-09 17:21:03 +0900

    🌱 Atm 기계 작업 완료
    try except 로 값을 정확히 입력할 수 있도록 예외처리를 했으며, 영수증은 receipts 리스트에 요소가 들어있는지 판별하여 예외처리

--- atm.py
+++ atm.py
@@ -7,32 +7,46 @@ receipts = []
 balance = 3000
 
 while True:
-    select_num = input("사용하실 기능을 선택해주세요. (1: 입금, 2: 출금, 3: 영수증 보기, 4: 종료)")
+    try:
+        select_num = int(input("사용하실 기능을 선택해주세요. (1: 입금, 2: 출금, 3: 영수증 보기, 4: 종료)"))
 
-    if select_num == "4":
-        break
-    elif select_num == "1":
-        deposit_amount = input("입금하실 금액을 입력해주세요 : ")
-        if deposit_amount.isdigit() and int(deposit_amount) > 0:
-            balance += int(deposit_amount)
-            receipts.append(("입금", deposit_amount, balance))
-            print(f'현재 입금액은 {deposit_amount}원\n현재 잔액은 {balance}원 입니다.')
+        if select_num == 4:
+            break
+        elif select_num == 1:  # 입금
+            try:
+                deposit_amount = int(input("입금하실 금액을 입력해주세요 : "))
+                if deposit_amount > 0:
+                    balance += deposit_amount
+                    receipts.append(("입금", deposit_amount, balance))
+                    print(f'현재 입금액은 {deposit_amount}원\n현재 잔액은 {balance}원 입니다.')
+                else:
+                    print("0 이상의 금액만 입력해주시기 바랍니다.")
+            except ValueError:
+                print("유효한 숫자를 입력해주세요.")
+        elif select_num == 2:  # 출금
+            try:
+                withdraw_amount = int(input("출금하실 금액을 입력해주세요 : "))
+                if withdraw_amount > 0:
+                    withdraw_amount = min(balance, withdraw_amount)
+                    balance -= withdraw_amount
+                    receipts.append(("출금", withdraw_amount, balance))
+                    print(f'현재 출금액은 {withdraw_amount}원\n현재 잔액은 {balance}원 입니다.')
+                else:
+                    print("0 이상의 금액만 입력해주시기 바랍니다.")
+            except ValueError:
+                print("유효한 숫자를 입력해주세요.")
+        elif select_num == 3:  # 영수증 보기
+            print("영수증")
+            if receipts:
+                for record in receipts:
+                    print(f"{record[0]}: {record[1]}원, 잔액: {record[2]}원")
+            else:
+                print("기록된 영수증이 없습니다.")
         else:
-            print(f'0 이상의 숫자만 입력해주시기 바랍니다.')
-    elif select_num == "2":
-        withdraw_amount = int(input("출금하실 금액을 입력해주세요 : "))
-        # if balance > withdraw_amount:
-        #     balance -= balance - withdraw_amount
-        # else:
-        #     balance = 0
-        # 또는 min() 함수를 통해 최솟값을 구할 수 있다.
-        withdraw_amount = min(balance, withdraw_amount)
-        balance -= withdraw_amount
-        receipts.append(("출금", withdraw_amount, balance))
-        print(f'현재 출금액은 {withdraw_amount}원\n현재 잔액은 {balance}원 입니다.')
-    else:
-        print(f'영수증\n{receipts}\n입니다.')
-        
+            print("유효한 메뉴 번호를 선택해주세요. (1, 2, 3, 4)")
+    except ValueError:
+        print("유효한 숫자를 입력해주세요. (1, 2, 3, 4)")
+
 print(f'서비스를 종료합니다. 현재 잔액은 {balance}원 입니다 ^^')
 
 
